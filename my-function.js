cb68cfcda4704c554d99329978e862277bba5651
kautilya <contact@kautilyak.dev>
2022-10-05 20:09:05 +0000

    add

--- my-function.js
+++ my-function.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const axios = require("axios");
+const {GoogleAuth} = require('google-auth-library');
+
 
 app.get('/', function(req, res) {
     res.json({status: 200});
@@ -13,16 +16,36 @@ app.get('/json', (req, res) => {
   });
 });
 
+
+
+const auth = new GoogleAuth({
+  scopes: 'https://www.googleapis.com/auth/cloud-platform'
+});
+
 app.get('/say', (req, res) => {
-  if (!req.query.keyword) {
-    res.send("No Keyword sent!");
-    return;
-  }
-  const {keyword} = req.query;
+  console.log(req.query.keyword)
+
   
-  res.status(200);
-  res.send(`kautilya says ${keyword}`);
-  return `kautilya says ${keyword}`;
+    let config = {
+      headers: {
+        "Authorization": "Basic ZTcwMzNhMDMtYmE0MS00YTg1LTg0N2EtMGZkODI2ZTI3MjdjOlBNbk1wZnRIM0tveDRwQ3Mxd3o3dmtxeGRMZjd5OUtkcWxSMnZFb3lNbnJNNjA3eTF0ZGtyTFNuVTFaTDZKVHA="
+      },
+      params: {
+        keyword: req.query.keyword
+      }
+    };
+
+    axios.get(`https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-e465d419-f31c-4981-a026-bd6ba69e27ca/x/func`, config)
+      .then((data) => {
+        console.log(data.data)
+        res.send(data.data)
+      })
+      .catch((err) => {
+        
+        res.send(err)
+        console.log(err)
+      })
+  
 });
 
 app.listen(port, () => {
